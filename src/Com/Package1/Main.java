1d427c0364dfdff9c09cd1141be6ea39cad66482
Rom.Solonchenko <RSolonchenko@turbosolution.ru>
2023-04-04 17:35:34 +0300

    Initial commit

--- src/Com/Package1/Main.java
+++ src/Com/Package1/Main.java
@@ -32,32 +32,43 @@ public class Main {
 //        int num1 = object1.nextInt();
 //        byte num2 = object1.nextByte();
 //        boolean boo1 = object1.nextBoolean();
-        System.out.println("Give me a first number : ");
-        float num1 = object1.nextFloat();
-
-//          float num1 = 50, num2 = 33;
-        System.out.println("Give me a second number :");
-        float num2 = object1.nextFloat();
-        float res1 = num1 % num2;
-        float res2 = num1 + num2;
-        float res3 = num1 - num2;
-        float res4 = num1 * num2;
-        float res5 = num1 / num2;
+//        System.out.println("Give me a first number : ");
+//        float num1 = object1.nextFloat();
+//
+////          float num1 = 50, num2 = 33;
+//        System.out.println("Give me a second number :");
+//        float num2 = object1.nextFloat();
+//        float res1 = num1 % num2;
+//        float res2 = num1 + num2;
+//        float res3 = num1 - num2;
+//        float res4 = num1 * num2;
+//        float res5 = num1 / num2;
 
 //          res += 10;
 //          res++;
-          System.out.println("Result : ");
-          System.out.println(res1 + "\n" + res2 + "\n" + res3 + "\n" + res4 + "\n" + res5 +"\n");
-          int FirstNumber = 10, SecondNumber = 15;
-          boolean log1 = true;
-        System.out.println("Give me first number for if : ");
-        float FirstNumberForIf = object1.nextFloat();
-        System.out.println("Give me a second number for if :");
-        float SecondNumberForIf = object1.nextFloat();
-          if ( log1 || FirstNumberForIf == SecondNumberForIf ) {
-              System.out.println("Uspehov");
-          } else {
-              System.out.println("Not uspehov");
-          }
+//          System.out.println("Result : ");
+//          System.out.println(res1 + "\n" + res2 + "\n" + res3 + "\n" + res4 + "\n" + res5 +"\n");
+//          int FirstNumber = 10, SecondNumber = 15;
+//        System.out.print("Give me first string for if(user role) : ");
+//        String FirstNumberForIf = object1.nextLine();
+//        System.out.print("Give me a second string for if(password) : ");
+//        String SecondStringForIf = object1.nextLine();
+//          if ( FirstNumberForIf.equals("Admin") && SecondStringForIf.equals("qwerty") ) {
+//              System.out.println("Uspehov");
+//          } else {
+//              System.out.println("Not uspehov");
+//          }
+
+        int num = object1.nextInt();
+        switch (num) {
+            case 1:
+                System.out.println("num == 1");
+                break;
+            case 2 :
+                System.out.println("num == 2");
+                break;
+            default:
+                System.out.println("lOH");
+        }
     }
 }
